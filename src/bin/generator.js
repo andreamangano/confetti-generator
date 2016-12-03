471ab4b6fee6cc0e73085b892b8d4e040965eaf0
AMangano <andreaman87@gmail.com>
2016-12-03 13:35:21 +0100

    Created bin file to use generator by cli.

--- src/bin/generator.js
+++ src/bin/generator.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 'use strict';
-// TODO
+import Generator from './../lib/generator';
+const stdin = process.stdin;
+let content = '';
+stdin.resume();
+stdin.setEncoding('utf8');
+stdin.on('data', buf => {
+  content += buf;
+});
+stdin.on('end', () => {
+  const data = JSON.parse(content);
+  const g = new Generator(data.paths, true);
+  g.compileViews(data);
+  console.log('data.theme: ' + data.theme);
+});
